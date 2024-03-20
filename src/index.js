f3bc864ddc1d72ba540cc5e6ae222d6e925f0d63
Everton Afonso <evertonafonso0@gmail.com>
2024-03-20 08:36:47 -0300

    feat: account route creation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(express.json());
+
+const custumers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const custumersAlreadyExists = custumers.some(
+    (custumer) => custumer.cpf === cpf
+  );
+
+  console.log(custumersAlreadyExists);
+
+  if (custumersAlreadyExists) {
+    return response.status(400).json({
+      error: "Customer already exists!",
+    });
+  }
+
+  custumers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
 app.listen(3333);
