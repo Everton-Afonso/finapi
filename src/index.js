f6a6741dd435a4f29cca43f5e730af9ec807484a
Everton Afonso <evertonafonso0@gmail.com>
2024-03-21 08:24:50 -0300

    feat: add deposit function and withdraw function

--- src/index.js
+++ src/index.js
@@ -22,13 +22,23 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
   const custumersAlreadyExists = custumers.some(
     (custumer) => custumer.cpf === cpf
   );
-
-  console.log(custumersAlreadyExists);
 
   if (custumersAlreadyExists) {
     return response.status(400).json({
@@ -52,6 +62,43 @@ app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
   const { custumer } = request;
 
   return response.json(custumer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { custumer } = request;
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  custumer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { custumer } = request;
+  const balance = getBalance(custumer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({
+      error: "Insufficient funds!",
+    });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  custumer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 app.listen(3333);
